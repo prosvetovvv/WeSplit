423268b2f9b0ddd99c7edbd2fe97cb360490cc34
prosvetovvv <vp12@yandex.ru>
2021-02-04 13:49:22 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vitaly Prosvetov on 04.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
